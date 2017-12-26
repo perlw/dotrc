11b6240e36ab67afda25d3488b61bebb71d3a528
Per Lönn Wege <per.loenn.wege@gmail.com>
2017-12-26 19:53:44 +0100

    i3wm config

---
+++ i3/multihead.sh
@@ -0,0 +1,4 @@
+#!/bin/zsh
+# TODO: Toggle
+xrandr --output eDP1 --auto --output HDMI1 --auto --left-of eDP1
+feh --bg-scale ~/.config/wall.png
---
+++ i3/net_wifi.sh
@@ -0,0 +1,4 @@
+#!/bin/zsh
+# TODO: Toggle
+systemctl start wpa_supplicant@wlp2s0
+systemctl start dhcpcd
