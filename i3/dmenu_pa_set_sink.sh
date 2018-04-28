7347d37ed3aa3ca293e00a689af040a64ead5f81
Per Lönn Wege <per@perlw.se>
2018-04-28 21:28:46 +0200

    Give title

--- i3/dmenu_pa_set_sink.sh
+++ i3/dmenu_pa_set_sink.sh
@@ -1,4 +1,4 @@
 #!/bin/zsh
 sinks=$(pacmd list-sinks | awk '/index:/{i=$NF}/device.description/{print i,":",substr($0, index($0, $3)+1, length($0)-index($0, $3)-1)};')
-choice=$(echo $sinks | dmenu)
+choice=$(echo $sinks | dmenu -i -p "Sinks")
 [[ ! -z $choice ]] && pacmd set-default-sink $(echo $choice | cut -d ' ' -f 1)
