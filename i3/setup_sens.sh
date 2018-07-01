9ff63687a50814d2542e93c20a47c66e8ecae207
Per Lönn Wege <per@perlw.se>
2018-07-01 13:28:49 +0200

    Update home sens

--- i3/setup_sens.sh
+++ i3/setup_sens.sh
@@ -1,3 +1,3 @@
 #!/bin/zsh
 xinput --set-prop 'Microsoft Microsoft 5-Button Mouse with IntelliEye(TM)' 'Coordinate Transformation Matrix' 2.5 0 0 0 2.5 0 0 0 1.5
-xinput --set-prop 'Logitech Gaming Mouse G400' 279 -0.6
+xinput --set-prop 'Logitech Gaming Mouse G400' 'libinput Accel Speed' -0.6
