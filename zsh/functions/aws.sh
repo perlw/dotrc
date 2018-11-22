d0b4493df184aca841435cc93cd5643bfe3eeb62
Per Lönn Wege <per@perlw.se>
2018-11-22 14:41:12 +0100

    More AWS helpers

--- zsh/functions/aws.sh
+++ zsh/functions/aws.sh
@@ -27,5 +27,45 @@ AWSkick() {
   fi
 }
 
+AWSstate() {
+  err=0
+  if [[ $# -gt 0 ]]; then
+    args=("${(s:/:)1}")
+    if [[ $#args -gt 1 ]]; then
+      aws ecs describe-services --cluster $args[1] --services $args[2] | jq -r .services[].events[0].message
+      err=$?
+    else
+      err=1
+    fi
+  else
+    err=1
+  fi
+
+  if [[ $err -eq 1 ]]; then
+    print -P '$0 <cluster>/<service>'
+  fi
+}
+
+AWStaskenv() {
+  err=0
+  if [[ $# -gt 0 ]]; then
+    args=("${(s:/:)1}")
+    if [[ $#args -gt 1 ]]; then
+			aws ecs describe-services --cluster $args[1] --service $args[2] | jq -r .services[].taskDefinition | xargs -I {} aws ecs describe-task-definition --task-definition {} | jq .taskDefinition.containerDefinitions[].environment
+      err=$?
+    else
+      err=1
+    fi
+  else
+    err=1
+  fi
+
+  if [[ $err -eq 1 ]]; then
+    print -P '$0 <cluster>/<service>'
+  fi
+}
+
 alias aws-env='setupAWSEnv'
 alias aws-kick='AWSkick'
+alias aws-state='AWSstate'
+alias aws-task-env='AWStaskenv'
