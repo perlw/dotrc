52e37f700d7f631a8ff72aee1dedfae0916e9325
Per Lönn Wege <per@perlw.se>
2019-02-15 15:31:59 +0100

    New work setup

--- i3/multihead.sh
+++ i3/multihead.sh
@@ -1,4 +1,4 @@
 #!/bin/zsh
 # TODO: Toggle
-xrandr --output eDP1 --auto --output HDMI1 --auto --left-of eDP1
+xrandr --output eDP1 --auto --output HDMI1 --auto --left-of eDP1 --output DP2 --auto --left-of HDMI1 --rotate right
 feh --bg-fill ~/.config/wall.png
