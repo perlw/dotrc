b01ec186c3778b3a982a8d567a820abe565600ad
Per Lönn Wege <per@perlw.se>
2019-04-23 15:55:48 +0200

    Check pipeline state

--- zsh/functions/aws.sh
+++ zsh/functions/aws.sh
@@ -92,8 +92,47 @@ AWSpipe() {
   fi
 }
 
+AWSlinestate() {
+  if [[ $# -gt 0 ]]; then
+    DATA=$(aws codepipeline get-pipeline-state --name $1)
+    if [[ $? -ne 0 ]]; then
+      print -P '%F{red}no such pipe%f'
+      return -1
+    fi
+
+    for STATE in $(echo $DATA | jq -cr ".stageStates[].actionStates[]"); do
+      NAME=$(echo $STATE | jq -r ".actionName")
+      STATUS=$(echo $STATE | jq -r ".latestExecution.status")
+      SUMMARY=$(echo $STATE | jq -r ".latestExecution.summary")
+      TIMESTAMP=$(echo $STATE | jq -r ".latestExecution.lastStatusChange" | xargs -I foo date -d @foo)
+
+      OUTPUT="$NAME ➡"
+      case $STATUS in
+        Succeeded)
+          OUTPUT="$OUTPUT %F{green}$STATUS%f"
+          ;;
+        "In Progress")
+          OUTPUT="$OUTPUT %F{yellow}$STATUS%f"
+          ;;
+        *)
+          OUTPUT="$OUTPUT %F{red}$STATUS%f"
+          ;;
+      esac
+      if [ $SUMMARY != "null" ]; then
+        OUTPUT="$OUTPUT\n├$SUMMARY"
+      fi
+      OUTPUT="$OUTPUT\n└$TIMESTAMP"
+
+      print -P "$OUTPUT\n"
+    done
+  else
+    print -P '$0 <pipeline>'
+  fi
+}
+
 alias aws-env='setupAWSEnv'
 alias aws-kick='AWSkick'
 alias aws-state='AWSstate'
 alias aws-task-env='AWStaskenv'
 alias aws-pipe='AWSpipe'
+alias aws-line-state='AWSlinestate'
