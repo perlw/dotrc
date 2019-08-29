5808f380b650487b260e13ecb2f5d8a3e499dd55
Per Lönn Wege <per@perlw.se>
2019-08-29 09:58:49 +0200

    Cleanup

--- zsh/init.sh
+++ zsh/init.sh
@@ -14,38 +14,13 @@ source "$PWD/functions/aur.sh"
 source "$PWD/functions/aws.sh"
 source "$PWD/functions/creds.sh"
 
-# Passwords
-alias sesame="eval \$(op signin my)"
-
-# Configs
-alias ez="nvim $HOME/.zshrc"
-alias ezi="nvim $HOMe/Projects/dotrc/zsh/init.sh"
-alias sz="source $HOME/.zshrc"
-alias ei="nvim $HOME/.config/i3/config"
-alias eis="nvim $HOME/.config/i3status/config"
-alias er="nvim $HOME/.config/ranger/rc.conf"
-
-# Navigation
-alias gd="cd $HOME/Downloads"
-alias gc="cd $HOME/.config"
-alias gci="cd $HOME/.config/i3"
-alias gtg="cd $GOPATH/src/github.com"
-alias gtgs="cd $GOPATH/src/github.com/Sydsvenskan"
-alias gtgp="cd $GOPATH/src/github.com/perlw"
-alias gp="cd $HOME/Projects"
-alias gw="cd $HOME/Work"
-alias gs="cd /storage"
-
 # Go
-alias gg="go get -v -u"
+alias gg="go get -v"
+alias gv="go mod tidy && go mod vendor"
 
 # Misc
 alias wttr="curl wttr.in/Malmö"
-alias dvim="DARK=1 $EDITOR"
 
 # Work
-alias ca="consul agent -server -data-dir . -bootstrap -advertise 127.0.0.1 -rejoin -ui"
-alias cg="cgateway proxy --omit jobinternalreport --omit apiproxy-80"
-alias cgp="cgateway proxy --production --omit jobinternalreport --omit apiproxy-80"
 alias wgu="wg-quick up ~/.config/wireguard/wg0.conf"
 alias wgd="wg-quick down ~/.config/wireguard/wg0.conf"
