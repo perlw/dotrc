e58be66067d3f6aacd8e0eae3c7bb9ccfdc30b81
Per Lönn Wege <per@perlw.se>
2019-11-18 15:19:58 +0100

    Set count

--- zsh/functions/aws.sh
+++ zsh/functions/aws.sh
@@ -167,10 +167,31 @@ AWSpipe() {
   fi
 }
 
+AWScount() {
+  err=0
+  if [[ $# -gt 0 ]]; then
+    args=("${(s:/:)1}")
+    if [[ $#args -gt 1 ]]; then
+      echo "Setting service count of $1 to $2"
+      aws ecs update-service --cluster $args[1] --service $args[2] --desired-count $2 > /dev/null
+      err=$?
+    else
+      err=1
+    fi
+  else
+    err=1
+  fi
+
+  if [[ $err -eq 0 ]]; then
+    print -P '%F{green}Done!%f'
+  else
+    print -P '$0 <cluster>/<service> <count>'
+  fi
+}
+
 alias aws-env='setupAWSEnv'
 alias aws-kick='AWSkick'
 alias aws-state='AWSstate'
 alias aws-task-env='AWStaskenv'
 alias aws-pipe='AWSpipe'
-
-# aws ecs update-service --cluster testing --service stableboy --desired-count 0
+alias aws-set-count='AWScount'
