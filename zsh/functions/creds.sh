613e111baa7243a7ec125e688b75ecf78fb201a7
Per Lönn Wege <per@perlw.se>
2020-02-07 10:56:08 +0100

    Toying

--- zsh/functions/creds.sh
+++ zsh/functions/creds.sh
@@ -5,4 +5,13 @@ genCreds() {
   secret=$(od -vN 16 -An -tx1 /dev/urandom | tr -d " \n"; echo)
   echo "{\"id\":\"${id}\",\"secret\":\"${secret}\"}"
 }
+
+jwtDec() {
+  echo "Header:"
+  echo $1 | cut -d "." -f 1 | base64 -i --decode | jq .
+  echo "Payload:"
+  echo $1 | cut -d "." -f 2 | base64 -i --decode | jq .
+}
+
 alias gen-creds='genCreds'
+alias jwt-dec='jwtDec'
