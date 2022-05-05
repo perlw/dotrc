888e8c0ef1b3ad06b8754c17313cd975b598486c
Per Lönn Wege <per@perlw.se>
2022-05-05 10:52:37 +0200

    zsh: oops, executing on shell launch..

--- zsh/functions/gcp.sh
+++ zsh/functions/gcp.sh
@@ -2,4 +2,4 @@
 
 alias gcp-builds="gcloud builds list --ongoing"
 alias gcp-builds-id="gcloud builds list --ongoing | cut -d ' ' -f 1 | tail -n 1"
-alias gcp-stream-build="gcloud builds log --stream `gcp-builds-id`"
+# alias gcp-stream-build="gcloud builds log --stream `gcp-builds-id`"
