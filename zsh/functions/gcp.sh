76c4f31c1b00a2d1d0a4a479679e15a4f34d2acc
Per Lönn Wege <per@perlw.se>
2022-06-07 10:54:10 +0200

    zsh: Fix gcp build streaming log

--- zsh/functions/gcp.sh
+++ zsh/functions/gcp.sh
@@ -1,5 +1,9 @@
 #!/bin/zsh
 
-alias gcp-builds="gcloud builds list --ongoing"
-alias gcp-builds-id="gcloud builds list --ongoing | cut -d ' ' -f 1 | tail -n 1"
-# alias gcp-stream-build="gcloud builds log --stream `gcp-builds-id`"
+GCPstreamTopBuild() {
+  gcloud beta builds log --stream `gcp-builds-id`
+}
+
+alias gcp-builds="gcloud beta builds list --ongoing"
+alias gcp-builds-id="gcloud beta builds list --ongoing | cut -d ' ' -f 1 | tail -n 1"
+alias gcp-stream-build="GCPstreamTopBuild"
