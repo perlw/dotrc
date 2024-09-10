0a591008dcdb81ac63aec76c44ec989359087ce2
Per Lönn Wege <per@perlw.se>
2024-09-10 13:16:53 +0200

    wezterm: make left option work as expected

--- wezterm/modules/mac_config.lua
+++ wezterm/modules/mac_config.lua
@@ -4,8 +4,9 @@ local module = {}
 
 function module.apply_to_config(config)
   config.font = wezterm.font 'IosevkaTerm NFM'
-  config.font_size = 18
+  config.font_size = 16
   config.default_prog = { 'zsh', '--login' }
+  config.send_composed_key_when_left_alt_is_pressed = true
 end
 
 return module
