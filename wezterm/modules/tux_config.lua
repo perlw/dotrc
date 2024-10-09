e1accda3567c6016f1150829d8a272c4ee483b80
Per Lönn Wege <per@perlw.se>
2024-10-09 12:09:41 +0200

    wezterm: adapt

--- wezterm/modules/tux_config.lua
+++ wezterm/modules/tux_config.lua
@@ -4,7 +4,7 @@ local module = {}
 
 function module.apply_to_config(config)
   config.font = wezterm.font 'IosevkaTerm NFM'
-  config.font_size = 16
+  config.font_size = 14
   config.default_prog = { '/usr/bin/zsh', '-l' }
   config.send_composed_key_when_left_alt_is_pressed = true
 end
